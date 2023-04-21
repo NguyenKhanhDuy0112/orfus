2e6983edbf17473afb21fcd656fb0c00c4ba1d62
NguyenKhanhDuy0112 <duynguyen.011202@gmail.com>
2023-04-21 13:22:56 +0700

    commit

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 // Import packages
+const cors = require('cors');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
 const express = require("express");
+
 const home = require("./routes/home");
 
-// Middlewares
 const app = express();
 app.use(express.json());
+
+app.use(bodyParser.json({ limit: '30mb' }));
+app.use(bodyParser.urlencoded({ extended: false, limit: '30mb' }));
+
+const corsOptions = {
+    origin: true,
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
+dotenv.config();
+// Middlewares
+
 
 // Routes
 app.use("/api/v1", home);
