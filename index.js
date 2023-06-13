04697827d282e9b2cb2ef099224fe30febda3be5
Sang Nguyen Minh <sang.nguyen@aegona.com>
2023-06-13 22:38:29 +0700

    .

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Import packages
-const cors = require('cors');
-const dotenv = require('dotenv');
-const bodyParser = require('body-parser');
+const cors = require("cors");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
 const express = require("express");
 
 const home = require("./routes/home");
@@ -9,22 +9,21 @@ const home = require("./routes/home");
 const app = express();
 app.use(express.json());
 
-app.use(bodyParser.json({ limit: '30mb' }));
-app.use(bodyParser.urlencoded({ extended: false, limit: '30mb' }));
+app.use(bodyParser.json({ limit: "30mb" }));
+app.use(bodyParser.urlencoded({ extended: false, limit: "30mb" }));
 
 const corsOptions = {
-    origin: true,
-    credentials: true,
+   origin: true,
+   credentials: true,
 };
 
 app.use(cors(corsOptions));
 dotenv.config();
 // Middlewares
 
-
 // Routes
 app.use("/api/v1", home);
 
-// connection
+// connections
 const port = process.env.PORT || 9001;
 app.listen(port, () => console.log(`Listening to port ${port}`));
