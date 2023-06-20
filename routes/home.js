b8b260a4059d7c604568232041c7c8ee02ea5d6f
Sang Nguyen Minh <sang.nguyen@aegona.com>
2023-06-20 14:30:07 +0700

    .

--- routes/home.js
+++ routes/home.js
@@ -61963,4 +61963,18 @@ router.post("/switch_display_wait_time", async (req, res, next) => {
    }
 });
 
+router.post("/ORB_content_to_display", async (req, res, next) => {
+   try {
+      const q = req.query.mock;
+      const findIndex = req.url.split("").findIndex((x) => x === "?");
+      const url = req.url.split("").slice(1, findIndex).join("");
+      if (Object.keys(orfusJson).includes(url)) {
+         return res.status(200).json(orfusJson[url][q]);
+      }
+      return res.status(400).json({});
+   } catch (error) {
+      return res.status(500).json({ message: "fail" });
+   }
+});
+
 module.exports = router;
